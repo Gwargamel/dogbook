b084a032e91f1d5fc185d47a188c8e31c21ef7fc
Gwargamel <119676411+Gwargamel@users.noreply.github.com>
2024-04-07 14:43:35 +0200

    Ändrade till dogbook utan stort D

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // Anslut till MongoDB-databasen
-mongoose.connect('mongodb://localhost:27017/Dogbook')
+mongoose.connect('mongodb://localhost:27017/dogbook')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
