628656b8aea7b39c87b933127003066921fc9cf1
Gwargamel <119676411+Gwargamel@users.noreply.github.com>
2024-04-13 20:54:48 +0200

    Uppdaterat kommentarer i Dog.js

--- models/Dog.js
+++ models/Dog.js
@@ -1,11 +1,15 @@
-//Dog.js
+// Importerar mongoose-paketet för att interagera med MongoDB
 import mongoose from 'mongoose';
 
+// Schema för 'Dog' som definierar strukturen för 'Dog'-dokument i databasen
 const DogSchema = new mongoose.Schema({
+  // Obligatoriska fält för hundarnas namn, ålder och beskrivning
   name: { type: String, required: true },
   age: { type: Number, required: true },
   description: String,
+  // Ett booleskt fält som automatiskt kommer att sättas till 'false' om det inget annat anges
   present: { type: Boolean, default: false },
+  // En array för att skapa relationer mellan olika 'Dog'-dokument.
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dog' }]
 });
 
