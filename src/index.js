aeb043483b449d90836b3a789a755c8133af8a04
Gwargamel <119676411+Gwargamel@users.noreply.github.com>
2024-04-14 18:33:59 +0200

    Lade til kommentarer i index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-//index.js
-
+// Importerar React-biblioteket för att använda React-funktioner och komponenter
 import React from 'react';
+// Importerar ReactDOM för att hantera DOM-relaterade renderingar, specifikt med React 18:s nya klient-API
 import ReactDOM from 'react-dom/client';
+// Importerar en CSS-fil för global styling som påverkar hela applikationen
 import './index.css';
+// Importerar huvudkomponenten 'App' från en fil i samma katalog
 import App from './App.js';
 
-
+// Skapar en root-container där React-applikationen kommer att fästas och renderas
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Renderar React-applikationen i root-elementet i strikt läge
 root.render(
+  // React.StrictMode är ett hjälpverktyg som aktiverar ytterligare kontroller och varningar för dess barn
   <React.StrictMode>
     <App />
   </React.StrictMode>
